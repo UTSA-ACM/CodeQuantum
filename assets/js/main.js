7e19216feb366814c03f5c10b4c2d3ab9a1f3b76
Zaquariah Holland <zaquariah@gmail.com>
2021-09-14 00:12:22 -0500

    FAQ update

--- assets/js/main.js
+++ assets/js/main.js
@@ -100,7 +100,23 @@
 			autoplay: true,
 			autoplaySpeed: 1500,
 			arrows: false,
-			initialSlide: randomFirstSlide()
+			initialSlide: randomFirstSlide(),
+			responsive: [
+				{
+					breakpoint: 1024,
+					settings: {
+						slidesToShow: 3,
+						slidesToScroll: 1,
+					}
+				},
+				{
+					breakpoint: 767,
+					settings: {
+						slidesToShow: 2,
+						slidesToScroll: 2,
+					}
+				}
+			]
 		});
 	});
 
